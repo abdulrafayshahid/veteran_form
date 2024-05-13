775402fe1828b32817e603e34945b0aab8b13c0e
Abdul Rafay <143813838+abdulrafayshahid@users.noreply.github.com>
2024-05-13 20:58:48 +0500

    questionnaire saving functionality added

--- backend/function.php
+++ backend/function.php
@@ -24,50 +24,29 @@ try {
     $mail->setFrom('noreply@progsportsdev.com', 'Mailer');
     $mail->addAddress('abdulrafay99910@gmail.com', 'Wide Awake Community'); // Add a recipient
 
-    $date = $_POST['date'];
-    $name = $_POST['name'];
-    $state = $_POST['state'];
-    $older_than_55 = $_POST['older_than_55'];
-    $hear_about_us = $_POST['hear_about_us'];
-    $seeking_resources = $_POST['seeking_resources'];
-    $dob = $_POST['dob'];
-    $street_address = $_POST['street_address'];
-    $phone = $_POST['phone'];
+    $first_name = $_POST['first_name'];
+    $middle_initial = $_POST['middle_initial'];
+    $last_name = $_POST['last_name'];
+    $cell_phone = $_POST['cell_phone'];
     $email = $_POST['email'];
-    $abundant_aging_question1 = $_POST['abundant_aging_question1'];
-    $abundant_aging_question2 = $_POST['abundant_aging_question2'];
-    $abundant_aging_question3 = $_POST['abundant_aging_question3'];
-    $volunteering_question1 = $_POST['volunteering_question1'];
-    $volunteering_question2 = $_POST['volunteering_question2'];
-    $volunteering_question3 = $_POST['volunteering_question3'];
-    $volunteering_question4 = $_POST['volunteering_question4'];
-    $kinship_family_question1 = $_POST['kinship_family_question1'];
-    $kinship_family_question2 = $_POST['kinship_family_question2'];
-    $kinship_family_question3 = $_POST['kinship_family_question3'];
-    $kinship_family_question4 = $_POST['kinship_family_question4'];
-    $care_services_question1 = $_POST['care_services_question1'];
-    $care_services_question2 = $_POST['care_services_question2'];
-    $care_services_question3 = $_POST['care_services_question3'];
-    $legal_decision_making_question1= $_POST['legal_decision_making_question1'];
-    $legal_decision_making_question2= $_POST['legal_decision_making_question2'];
-    $legal_decision_making_question3= $_POST['legal_decision_making_question3'];
-    $legal_decision_making_question4= $_POST['legal_decision_making_question4'];
-    $legal_decision_making_question5= $_POST['legal_decision_making_question5'];
-    $legal_decision_making_question6= $_POST['legal_decision_making_question6'];
-    $lss_meals_question1 = $_POST['lss_meals_question1'];
-    $lss_meals_question2 = $_POST['lss_meals_question2'];
-    $lss_meals_question3 = $_POST['lss_meals_question3'];
-    $lss_meals_question4 = $_POST['lss_meals_question4'];
-    $lss_meals_question5 = $_POST['lss_meals_question5'];
-    $volunteer_paid_opportunities_question1 = $_POST['volunteer_paid_opportunities_question1'];
-    $volunteer_paid_opportunities_question2 = $_POST['volunteer_paid_opportunities_question2'];
-    $volunteer_paid_opportunities_question3 = $_POST['volunteer_paid_opportunities_question3'];
-    $volunteer_paid_opportunities_question4 = $_POST['volunteer_paid_opportunities_question4'];
-    $volunteer_paid_opportunities_question5 = $_POST['volunteer_paid_opportunities_question5'];
+    $address = $_POST['address'];
+    $homeless = $_POST['homeless'];
+    $your_description = $_POST['your_description'];
+    $other_details = $_POST['other_details'];
+    $homeless2 = $_POST['homeless2'];
+    $sex = $_POST['sex'];
+    $age = $_POST['age'];
+    $race = $_POST['race'];
+    $hear_about_us = $_POST['hear_about_us'];
+    $county_residence = $_POST['county_residence'];
+    $work_hours = $_POST['work_hours'];
+    $school_certification = $_POST['school_certification'];
+    $learn_about_services = $_POST['learn_about_services'];
+    $learn=implode(',',$learn_about_services);
 
     // Prepare SQL statement to insert data into the table
-    $sql = "INSERT INTO questionnaire (`date`, `name`, `state`, `older_than_55`, `hear_about_us`, `seeking_resources`, `dob`, `street_address`, `phone`, `email`) 
-            VALUES ('$date', '$name', '$state', '$older_than_55', '$hear_about_us', '$seeking_resources', '$dob', '$street_address', '$phone', '$email')";
+    $sql = "INSERT INTO questionnaire (`first_name`, `middle_initial`, `last_name`, `cell_phone`, `email`, `address`, `homeless`, `your_description`, `other_details`, `homeless2`,`sex`,`age`,`race`,`hear_about_us`,`county_residence`,`work_hours`,`school_certification`,`learn_about_services`) 
+            VALUES ('$first_name', '$middle_initial', '$last_name', '$cell_phone', '$email', '$address', '$homeless', '$your_description', '$other_details', '$homeless2','$sex','$age','$race','$hear_about_us','$county_residence','$work_hours','$school_certification','$learn')";
 
     $result = $conn->query($sql);
 
